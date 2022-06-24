4191a06e4fd77d4ad00790f2c58f8230f1794066
Jwell2014 <91597225+Jwell2014@users.noreply.github.com>
2022-06-24 10:49:34 +0200

    securisation

--- src/Controller/DossierController.php
+++ src/Controller/DossierController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\Dossier;
 use App\Form\DossierType;
 use App\Repository\DossierRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[isGranted("ROLE_ADMIN")]
 #[Route('/dossier')]
 class DossierController extends AbstractController
 {
